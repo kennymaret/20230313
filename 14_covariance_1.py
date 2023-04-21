6e8bd2a1571f8e5dbfd70a30f1947fa76e20f219
Kenny <Kenny.sw1999@gmail.com>
2023-04-21 13:34:13 +0800

    Assignment 14_covariance_1

---
+++ 14_covariance_1.py
@@ -0,0 +1,29 @@
+#
+# Covariance for Python program
+#
+import statistics, math
+
+def my_covariance(input_x, input_y):
+    cov = 0 
+    length= len(input_x)
+
+    mean_x= statistics.mean(input_x)
+    mean_y= statistics.mean(input_y)
+    
+    cov= sum([(input_x[index]-mean_x)*(input_y[index]-mean_y) for index in range(length)])/length
+
+    print(f'Input x:{input_x}, Input y: {input_y}')
+    print(f'Mean x : {statistics.mean(input_x)}, Mean y : {statistics.mean(input_y)} ')
+    
+    return cov
+
+# 1. Input
+input_x = [10,20,30]
+input_y = [12,21,33]
+
+# 2. Process
+answer = my_covariance(input_x, input_y)
+answer = round(answer, 2)
+
+# 3. Output
+print(f'Answer: {answer}')
